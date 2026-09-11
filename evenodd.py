65c4caef69634490dfc4aa9c5668f8e0faa364b0
misujal04 <sujalghadge8@gmail.com>
2026-09-11 07:42:15 -0700

    End of 11th sept

--- evenodd.py
+++ evenodd.py
@@ -2,3 +2,8 @@
 a=int(input("Enter the number: "))
 
 print("Number is even:", a % 2 == 0)
+
+if a%2==0:
+    print("the number is even")
+else:
+    print("The number is odd")
---
+++ week.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the numbert:"))
+if(a==1):
+    print("Monday")
+elif(a==2):
+    print("Tuesday")
+elif(a==3):
+    print("Wednesday")
+elif(a==4):
+    print("Thursday")
+elif(a==5):
+    print("Friday")
+elif(a==6):
+    print("Saturday")
+elif(a==7):
+    print("Sunday")
+else:
+    print("put a valid numbetr between 1 to 7")
